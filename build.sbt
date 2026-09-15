b1e3ff46041eb34469b15f1fd033c36492a05ee6
krasov <magneticmechanicals@gmail.com>
2026-09-15 10:35:22 +0300

    feat(app): setup basic server

--- build.sbt
+++ build.sbt
@@ -1,9 +1,16 @@
 ThisBuild / scalaVersion := "3.9.0"
 
+lazy val catsEffectVersion = "3.7.1"
+lazy val http4sVersion = "0.23.37"
+
 lazy val root = (project in file("."))
-    .settings(
-        name := "ws-chat",
-        organization := "otus",
-        version := "0.1"
+  .settings(
+    name := "ws-chat",
+    organization := "otus",
+    version := "0.1",
+    libraryDependencies ++= Seq(
+      "org.typelevel" %% "cats-effect" % catsEffectVersion,
+      "org.http4s" %% "http4s-ember-server" % http4sVersion,
+      "org.http4s" %% "http4s-dsl" % http4sVersion
     )
-    
+  )
--- src/main/scala/chat/Main.scala
+++ src/main/scala/chat/Main.scala
@@ -1,6 +1,24 @@
 package chat
 
-@main
-def main(): Unit = {
-    println("hello, world")
+import cats.effect.{IO, IOApp}
+import com.comcast.ip4s.*
+import org.http4s.dsl.io.*
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.HttpRoutes
+import org.http4s.Request
+
+object Main extends IOApp.Simple {
+  private val routes = HttpRoutes.of[IO] {
+    case GET -> Root / "health" =>
+      Ok("ok")
+  }
+  override def run: IO[Unit] = {
+    EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"127.0.0.1")
+      .withPort(port"8080")
+      .withHttpApp(routes.orNotFound)
+      .build
+      .useForever
+  }
 }
